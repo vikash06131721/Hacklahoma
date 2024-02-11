ab8f331884a015496860a24a1d9558733f23a882
vikash06131721 <prasad.vikash05@gmail.com>
2024-02-11 10:44:02 -0600

    imp

--- AI Doctor/app/views.py
+++ AI Doctor/app/views.py
@@ -23,11 +23,21 @@ def get_content():
         past = request.form.get('presentHis')
         present = request.form.get('pastHis')
         limit = request.form.get("myRange")
-        data['past'] = past
-        data['present']= present
+        # import pdb;pdb.set_trace()
+        # data['past'] = past
+        # data['present']= present
+        if past!="" or past!=None:
+            symptom = symptom+ "past illness "+past
+        if present!="" or present!=None: 
+            symptom = symptom+ "present illness "+present
+        
         params = {
-            'param': symptom,
+            'instruction': "You are a helpful, respectful, and honest assistant. Always answer as helpfully as possible while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don’t know the answer to a question, please don’t share false information.",
+            'input':symptom,
+            'tokens':int(limit)
+
         }
+        # import pdb;pdb.set_trace()
         print(symptom)
         print(present)
         print(past)
