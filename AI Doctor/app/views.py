c5ab6872fb052b947a1d631451bf72a4ddc2e188
vikash06131721 <prasad.vikash05@gmail.com>
2024-02-10 20:09:47 -0600

    imp

--- AI Doctor/app/views.py
+++ AI Doctor/app/views.py
@@ -20,8 +20,11 @@ def get_content():
     if request.method == 'POST':
         symptom = request.form.get('Symptoms')
         params = {
-            'param': symptom,
+            'instruction': "You are a helpful, respectful, and honest assistant. Always answer as helpfully as possible while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don’t know the answer to a question, please don’t share false information.",
+            'input':symptom
         }
+        print(symptom)
+        # import pdb;pdb.set_trace()
         api_url = 'http://127.0.0.1:9000/return_pred'
         try:
             response = requests.post(api_url, json=params)
